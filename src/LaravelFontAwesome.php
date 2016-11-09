c85eddb2e188773995c3910f7892343698eaa5bb
Joshua de Gier <info@pendo.nl>
2016-11-09 20:37:09 +0000

    Applied fixes from StyleCI

--- src/LaravelFontAwesome.php
+++ src/LaravelFontAwesome.php
@@ -4,7 +4,6 @@ namespace PendoNL\LaravelFontAwesome;
 
 class LaravelFontAwesome
 {
-
     public function icon($icon, $options = [])
     {
         $options = $this->getOptions($options);
@@ -20,11 +19,12 @@ class LaravelFontAwesome
      * Strip the default fa- prefix from the icon name if provider.
      *
      * @param $icon
+     *
      * @return string
      */
     protected function getIcon($icon)
     {
-        return "fa-" . str_replace("fa-", "", $icon);
+        return 'fa-'.str_replace('fa-', '', $icon);
     }
 
     /**
@@ -32,14 +32,14 @@ class LaravelFontAwesome
      * string and assume it is an extra class name.
      *
      * @param $options
+     *
      * @return array
      */
     protected function getOptions($options)
     {
-        if(!is_array($options))
-        {
+        if (!is_array($options)) {
             $options = [
-                'class' => (string)$options
+                'class' => (string) $options,
             ];
         }
 
@@ -51,36 +51,36 @@ class LaravelFontAwesome
      *
      * @param $icon
      * @param $extraClasses
+     *
      * @return string
      */
     protected function buildIconClasses($icon, $extraClasses)
     {
-        return 'fa ' . $this->getIcon($icon) . ( $extraClasses != "" ? ' ' . $extraClasses : '' );
+        return 'fa '.$this->getIcon($icon).($extraClasses != '' ? ' '.$extraClasses : '');
     }
 
     /**
-     * Build the attribute list to add to the icon we are about to generate
+     * Build the attribute list to add to the icon we are about to generate.
      *
      * @param $options
      * @param $classes
+     *
      * @return string
      */
     protected function buildAttributes($options, $classes)
     {
         $attributes = [];
-        $attributes[] = 'class="' . $classes .'"';
+        $attributes[] = 'class="'.$classes.'"';
 
         unset($options['class']);
 
-        if(is_array($options))
-        {
-            foreach($options as $attribute => $value)
-            {
+        if (is_array($options)) {
+            foreach ($options as $attribute => $value) {
                 $attributes[] = $this->createAttribute($attribute, $value);
             }
         }
 
-        return (count($attributes) > 0) ? implode(' ', $attributes) : '' ;
+        return (count($attributes) > 0) ? implode(' ', $attributes) : '';
     }
 
     /**
@@ -88,22 +88,23 @@ class LaravelFontAwesome
      *
      * @param $attribute
      * @param $value
+     *
      * @return string
      */
     protected function createAttribute($attribute, $value)
     {
-        return $attribute . '="' . $value . '"';
+        return $attribute.'="'.$value.'"';
     }
 
     /**
-     * Build the icon with the correct attributes
+     * Build the icon with the correct attributes.
      *
      * @param $attributes
+     *
      * @return string
      */
     protected function buildIcon($attributes)
     {
-        return '<i ' . $attributes .'></i>';
+        return '<i '.$attributes.'></i>';
     }
-
 }
--- src/LaravelFontAwesomeServiceProvider.php
+++ src/LaravelFontAwesomeServiceProvider.php
@@ -6,40 +6,36 @@ use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 /**
- * Class LaravelFontAwesomeServiceProvider
- * @package PendoNL\LaravelFontAwesome
+ * Class LaravelFontAwesomeServiceProvider.
  */
 class LaravelFontAwesomeServiceProvider extends ServiceProvider
 {
     /**
      * Boot method registers the blade directive.
-     * Usage; @fa('list', ['attributes' => 'go here'])
+     * Usage; @fa('list', ['attributes' => 'go here']).
      *
      * @return void
      */
     public function boot()
     {
-        Blade::directive('fa', function($arguments) {
-            list($icon, $attributes) = explode(',', str_replace(['(',')',' ', "'"], '', $arguments), 2);
+        Blade::directive('fa', function ($arguments) {
+            list($icon, $attributes) = explode(',', str_replace(['(', ')', ' ', "'"], '', $arguments), 2);
 
             $options = [];
 
-            if($attributes != "")
-            {
-                $rawAttributes = str_replace(['array(', '[', ']', ')'], "", $attributes);
-                $arrAttributes = explode(",", $rawAttributes);
+            if ($attributes != '') {
+                $rawAttributes = str_replace(['array(', '[', ']', ')'], '', $attributes);
+                $arrAttributes = explode(',', $rawAttributes);
 
-                if(count($arrAttributes) > 0)
-                {
-                    foreach($arrAttributes as $string)
-                    {
-                        $attr = explode("=>", $string);
+                if (count($arrAttributes) > 0) {
+                    foreach ($arrAttributes as $string) {
+                        $attr = explode('=>', $string);
                         $options[$attr[0]] = $attr[1];
                     }
                 }
             }
 
-            return (new LaravelFontAwesome)->icon($icon, $options);
+            return (new LaravelFontAwesome())->icon($icon, $options);
         });
     }
 
